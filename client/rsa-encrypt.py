fd500a0de28d1ce09ab7f45d7b71db6f8a6a29da
nickolaslivero <njsl.snf21@uea.edu.br>
2024-01-06 15:35:20 -0400

    encrypt rsa

---
+++ client/rsa-encrypt.py
@@ -0,0 +1,45 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+def generate_key_pair():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+    )
+    public_key = private_key.public_key()
+    return private_key, public_key
+
+def encrypt_string(string_to_encrypt, public_key):
+    message = string_to_encrypt.encode('utf-8')
+    encrypted = public_key.encrypt(
+        message,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return encrypted
+
+def decrypt_string(encrypted_string, private_key):
+    decrypted = private_key.decrypt(
+        encrypted_string,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return decrypted.decode('utf-8')
+
+# Example:
+private_key, public_key = generate_key_pair()
+text_to_encrypt = "SEBA"
+
+encrypted_text = encrypt_string(text_to_encrypt, public_key)
+print("Texto criptografado:", encrypted_text)
+
+decrypted_text = decrypt_string(encrypted_text, private_key)
+print("Texto descriptografado:", decrypted_text)
